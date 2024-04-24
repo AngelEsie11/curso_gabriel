5bbb7517eb4f015e6bb788e07533176996c14f69
Angel <angelestebansierragonzalez@gmail.com>
2024-04-23 23:45:51 -0400

    trabajando json.py

--- python/comienos_j.py
+++ python/comienos_j.py
@@ -1,50 +1,10 @@
 import json
 
-json_str='''{
-  "squadName": "Super hero squad",
-  "homeTown": "Metro City",
-  "formed": 2016,
-  "secretBase": "Super tower",
-  "active": true,
-  "members": [
-    {
-      "name": "Molecule Man",
-      "age": 29,
-      "secretIdentity": "Dan Jukes",
-      "powers": [
-        "Radiation resistance",
-        "Turning tiny",
-        "Radiation blast"
-      ]
-    },
-    {
-      "name": "Madame Uppercut",
-      "age": 39,
-      "secretIdentity": "Jane Wilson",
-      "powers": [
-        "Million tonne punch",
-        "Damage resistance",
-        "Superhuman reflexes"
-      ]
-    },
-    {
-      "name": "Eternal Flame",
-      "age": 1000000,
-      "secretIdentity": "Unknown",
-      "powers": [
-        "Immortality",
-        "Heat Immunity",
-        "Inferno",
-        "Teleportation",
-        "Interdimensional travel"
-      ]
-    }
-  ]
-}'''
-#print(json_str)
-#print(type(json_str))
+json_str='{  "squadName": "Super hero squad",  "homeTown": "Metro City",  "formed": 2016,  "secretBase": "Super tower", "active": true,  "members": [    {      "name": "Molecule Man",      "age": 29,      "secretIdentity": "Dan Jukes",      "powers": [        "Radiation resistance",        "Turning tiny",        "Radiation blast"      ]    },    {      "name": "Madame Uppercut",      "age": 39,      "secretIdentity": "Jane Wilson",      "powers": [        "Million tonne punch",        "Damage resistance",        "Superhuman reflexes"      ]    },    {      "name": "Eternal Flame",      "age": 1000000,      "secretIdentity": "Unknown",      "powers": [        "Immortality",        "Heat Immunity",        "Inferno",        "Teleportation",        "Interdimensional travel"      ]    }  ]}'
+print(json_str)
+print(type(json_str))
 
-#python_dict=json.loads(json_str)
+python_dict=json.loads(json_str)
 
-#print(python_dict)
-#print(type(python_dict))
+print(python_dict)
+print(type(python_dict))
