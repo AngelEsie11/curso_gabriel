d47a1950f83bad24c22411c75b74077a7d5c420c
Angel E. Sierra Gonzalez <156468801+AngelEsie11@users.noreply.github.com>
2024-05-13 19:26:15 -0400

    conexion con la base de datos

--- python/db.py
+++ python/db.py
@@ -1,18 +1,11 @@
 import pyodbc
-import pandas as pd
 
-#voy a crear cuatro variables donde guardare los datos de conexion de mi servidor sql server
-server= 'MSI-1'
-bd= 'angel'
-user='angel'
-password='Eren2111'
+
+
 
 try:
-    conexion = pyodbc.connect('driver={obdc driver 17 for sql server};server='+server+';database='+bd+';uid='+user+';pwd='+password)
+    connection=pyodbc.connect('DRIVER={SQL server};SERVER=MSI-1;DATABASE=angel;uid=;pwd=')
     print('Conexion exitosa')
     
-except:
-    print('error al intentar')
-    
-    
-print(pyodbc.drivers())
+except Exception as ex:
+    print(ex)
