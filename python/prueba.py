95145b178a64b0366e3a29b54a6462ac392ba132
angel <angelestebansierragonzalez@gmail.com>
2024-05-28 23:04:04 -0400

    seguimos

--- python/prueba.py
+++ python/prueba.py
@@ -1,43 +1,18 @@
+import sys
 import pyodbc
 
-continual = "S"
-detener = "N"
-Counter = 10
+# connection parameters
+nHost = 'msi-1'
+nBase = 'INVENTARIO_ELECTRONICO'
+nUser = ''
+nPasw = ''
+
 
 try:
-    connection = pyodbc.connect('DRIVER={SQL Server};SERVER=MSI-1;DATABASE=angel;trusted_connection=yes;')
+    connection =  pyodbc.connect('DRIVER={SQL Server};SERVER='+nHost+';DATABASE='+nBase+';UID='+nUser+';PWD='+nPasw)
     print('Conexion exitosa')
 
-    cursor = connection.cursor()
-    cursor.execute("articulos_con_nombresprovedor")
-    rows = cursor.fetchall()
     
-    count = 0  
-
-    while True:
-        for row in rows:
-            print(row)  
-            count += 1 
-            
-            # ELegir la selecion ya sea continual o salir
-            if count >= Counter:
-                pregu = input("Presione S para continuar o N para salir: ").strip().upper()
-                if pregu == continual:
-                    count = 0  # resetiar el contador
-                elif pregu == detener:
-                    print("termino el proceso")
-                    break  #Detener el bucle
-                else:
-                    print("Entrada no valida. Saliendo del programa.")
-                    break
-
-        # aqui sale del bucle exterior
-        if count < Counter or pregu == detener or pregu not in (continual, detener):
-            break
-
-    cursor.close()
-    connection.close()
 
 except Exception as ex:
     print(f"Error: {ex}")
-
